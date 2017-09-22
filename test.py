Fixed derivatives.

test.py
@@ -63,7 +63,7 @@ def load_sfac():
 
     int_fn(sfac.sfac_ctor, sfac_p, nparr_t(6), np_intarr_t(3), c_int)
     int_fn(sfac.sfac_dtor, sfac_p)
-    int_fn(sfac.set_A, sfac_p, rad_fn, c_double, c_void_p)
+    int_fn(sfac.set_A, sfac_p, rad_fn, c_void_p)
     void_fn(sfac.set_L, sfac_p, nparr_t(6))
     void_fn(sfac.sfac, sfac_p, c_int, nparr, nparr)
     dbl_fn(sfac.en, sfac_p)
@@ -105,18 +105,17 @@ class Sfac:
     def __del__(self):
         self.sfac_dtor()
 
-    def set_A(self, f, R):
+    def set_A(self, f):
         self.f = f
-        self.R = R
 
-        n = self._sfac.set_A(self._s, self.rad_fn(f), R, cast(0, c_void_p))
+        n = self._sfac.set_A(self._s, self.rad_fn(f), cast(0, c_void_p))
         if n != 0:
             raise RuntimeError, "Error (%d) setting A-array."%n
 
     def set_L(self, L):
         self._sfac.set_L(self._s, L)
         if hasattr(self, "f"):
-            self.set_A(self.f, self.R)
+            self.set_A(self.f)
 
 eta = 0.5
 _fac = -(pi/eta)**2
@@ -152,16 +151,17 @@ def test_lines(M, Q, crds):
           np.abs(M[0,0] - sz).max()
 
 def test():
-    L = np.array([10., 10., 10., 0., 0., 0.])
+    L = np.array([10., 10., 10., 2., -0.1, 1.0])
+    #L = np.array([9.,9.,9.,0.,0.,0.])
     V = L[0]*L[1]*L[2]
     #N = 4
     #crds = random((N,3))
-    #crds = np.array([[0.1, 0.5, 0.1],
-    #                 [0.9, 0.5, 0.1],
-    #                 [0.1, 0.5, 0.9],
-    #                 [0.9, 0.5, 0.9]])
-    crds = np.array([[0.0, 0.0, 0.0],
-                     [0.0, 0.0, 0.2]])
+    crds = np.array([[0.1, 0.5, 0.1],
+                     [0.9, 0.4, 0.1],
+                     [0.1, 0.4, 0.9],
+                     [0.9, 0.5, 0.9]])
+    #crds = np.array([[0.1, 0.5, 0.9],
+    #                 [0.7, 0.0, 0.3]])
     N = len(crds)
     atoms = np.dot(crds, LofS(L))
     dx = np.zeros((N,3))
@@ -171,21 +171,37 @@ def test():
 
     Ecorr = np.sum(q*q)*(-eta/np.sqrt(pi))
 
-    s = Sfac(L, np.array([10,10,10], dtype=np.int), 6)
-    s.set_A(ewald_f, 0.5)
+    s = Sfac(L, np.array([10,10,10], dtype=np.int), 4)
+    s.set_A(ewald_f)
     s.sfac(N, q, atoms)
     test_lines(s.S(), q, crds)
+
+    def Ex(x):
+        s.sfac(N, q, x)
+        return s.en()
+    dx0 = num_diff(Ex, atoms)
+
+    s.sfac(N, q, atoms)
     print s.en()
     print s.de1(vir)
-    print vir
+    s.de2(N, q, atoms, dx)
+    print dx
+    print dx0
 
+    print vir
     def E(L):
         s.set_L(L)
-        s.sfac(N, q, np.dot(crds, LofS(L)))
+        #s.sfac(N, q, np.dot(crds, LofS(L)))
         return s.en()
-    Pi = SofL( np.dot(LofS(num_diff(E, L)).transpose(), LofS(L)) )/(-V)
+    s.sfac(N, q, atoms)
+    Pi = LofS(num_diff(E, L)).transpose()
+    # Pi is properly symmetric, but we're only computing the upper-diagonal.
+    Pi = SofL(np.dot(Pi, LofS(L)).transpose())/(-V)
     print Pi
-    for zi in np.arange(200)*0.1 - 10.05:
+
+    atoms *= 0.0
+    #for zi in np.arange(200)*0.1 - 10.05:
+    for zi in []:
         atoms[1,2] = zi
         s.sfac(N, q, atoms)
         en = s.de1(vir)
@@ -196,12 +212,12 @@ def test():
         r3 = np.sqrt(np.sum((atoms[1]-atoms[0]+array([0., 0., 10.]))**2))
 
         #Ereal = 0.5*( erfc(eta*r1)/r1 + erfc(eta*r2)/r2 + erfc(eta*r3)/r3 )
-        #en += Ereal+Ecorr
+        en += Ecorr
         print zi, en, dx[0,2], dx[1,2], dx[1,0], dx[1,1]
 
 def num_diff(f, x, h = 1e-7):
     ih = 1./(2.0*h)
-    h = 2./ih
+    h = 0.5/ih
 
     s = x.shape
     x = np.reshape(x, -1)