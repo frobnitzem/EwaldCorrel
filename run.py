54c3f6e0c8c38148dcfc8a2106b2e54479c61e64
David M. Rogers <predictivestatmech@gmail.com>
2025-06-06 17:58:18 -0400

    Added run - a simple interface to generate scattering plot for 1 structure.

---
+++ run.py
@@ -0,0 +1,46 @@
+import os, sys
+from ewald import Sfac, LofS, SofL, ewald_f, eta
+from math import pi, exp, erfc
+import numpy as np
+
+bfac = {
+  'H': -3.7390,
+  'C':  6.6460,
+  'O':  5.803,
+}
+
+def read_mol(fname):
+    names = []
+    crd = []
+    with open(fname, "r") as f:
+        lines = f.read().split("\n")
+        for line in lines[2:]:
+            tok = line.split()
+            if len(tok) >= 4:
+                names.append(tok[0])
+                crd.append(list(map(float, tok[1:4])))
+
+    return names, np.array(crd)
+
+def run(inp, out):
+    # Define the molecule.
+    names, crd = read_mol(inp)
+    wts = np.array([bfac[name] for name in names])
+    L = 11.680933*5
+
+    K = 128
+    s = Sfac(np.array([L,L,L,0,0,0]), np.array([K, K, K], dtype=int), 4)
+    #s.set_A(ewald_f)
+    Q = np.zeros((K, K, s.ldim), np.complex128)
+    S = 0
+
+    for frame in [crd]:
+        s.sfac(wts, crd)
+        Q += np.abs(s.get_S())**2
+
+    np.save(out, Q)
+
+if __name__=="__main__":
+    import sys
+    assert len(sys.argv) == 3, "Usage: %s <inp.xyz> <out.npy>"%sys.argv[0]
+    run(sys.argv[1], sys.argv[2])
